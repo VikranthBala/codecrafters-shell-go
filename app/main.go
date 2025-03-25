6e3275863ecabe441b821f431bbf4bd986704653
Vikranth Kumar <vikranthkumar0123@gmail.com>
2025-03-26 01:02:18 +0530

    add echo command

--- app/main.go
+++ app/main.go
@@ -19,15 +19,21 @@ func main() {
 		}
 		command = strings.TrimRight(command, "\n")
 
-		inpArgs := strings.Fields(command)
-		if inpArgs[0] == "exit" {
+		inpArgs := strings.Split(command, " ")
+
+		switch inpArgs[0] {
+		case "exit":
 			stsCode, err := strconv.Atoi(inpArgs[1])
 			if err != nil {
 				log.Fatal(err)
 			}
 			os.Exit(stsCode)
+		case "echo":
+			fmt.Println(strings.Join(inpArgs[1:], " "))
+		case "":
+			return
+		default:
+			fmt.Println(inpArgs[0] + ": command not found")
 		}
-		fmt.Println(command + ": command not found")
 	}
-
 }
